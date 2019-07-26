fbb53d1709752479e689ecf1e8871c22c39bac91
gelenaj@gmail.com <19tech19>
2019-07-25 23:47:57 -0500

    feat: Configure app for Firebase authentication

---
+++ app/src/main/java/com/example/instahappy/fragments/AlertDialogFragment.java
@@ -0,0 +1,22 @@
+package com.example.instahappy.fragments;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.os.Bundle;
+
+import com.example.instahappy.R;
+
+public class AlertDialogFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Context context = getActivity();
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+
+        builder.setTitle(R.string.error_title)
+                .setMessage(R.string.error_message)
+                .setPositiveButton(R.string.close_dialog_title, null);
+        return  builder.create();
+    }
+}
