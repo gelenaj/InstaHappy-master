bb6cc54e7d1f6f98c72ef18c778fa83a52261d38
gelenaj@gmail.com <19tech19>
2019-07-28 22:14:03 -0400

    feat: add navigation drawer

--- app/src/main/java/com/example/instahappy/SigninActivity.java
+++ app/src/main/java/com/example/instahappy/SigninActivity.java
@@ -1,22 +1,36 @@
 package com.example.instahappy;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 /**
  * A login screen that offers login via email/password.
  */
-public class SigninActivity extends AppCompatActivity implements OnClickListener {
-
+public class SigninActivity extends AppCompatActivity implements OnClickListener,
+GoogleApiClient.OnConnectionFailedListener{
+    private static final int SIGN_IN_REQUEST = 9001;
     private SignInButton mSignInButton;
     private GoogleApiClient mGoogleApiClient;
     private FirebaseAuth mFirebaseAuth;
@@ -34,12 +48,81 @@ public class SigninActivity extends AppCompatActivity implements OnClickListener
                 .requestEmail()
                 .requestProfile()
                 .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
         mFirebaseAuth = FirebaseAuth.getInstance();
     }
 
     @Override
     public void onClick(View v) {
+        if(v.getId() == R.id.signinButton){
+            signIn();
+        }
+    }
 
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, SIGN_IN_REQUEST);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(this, "Google Play Services Error", Toast.LENGTH_LONG).show();
+        //TODO: log to crash analytics
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == SIGN_IN_REQUEST){
+            GoogleSignInResult result =Auth.GoogleSignInApi
+                    .getSignInResultFromIntent(data);
+
+            if(result.isSuccess()){
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+            }
+
+            //TODO: else block--log in crash analytics
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getId(),
+                null);
+        mFirebaseAuth.signInWithCredential(credential)
+                .addOnFailureListener(this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+//TODO: handle error with Crash Analytics
+                    }
+                })
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            handleFirebaseAuthResult(task.getResult());
+                        }else{
+                            //TODO: log error in crash analytics
+                        }
+                    }
+                });
+
+    }
+
+    private void handleFirebaseAuthResult(AuthResult result) {
+        if(result!=null){
+            Toast.makeText(this, "Firebase Authentication suceeded", Toast.LENGTH_LONG).show();
+
+            //go back to main actity
+            Intent main = new Intent(this, MainActivity.class);
+            main.putExtra("SIGN_IN_KEY", true);
+            finish();
+        }
     }
 }
 
